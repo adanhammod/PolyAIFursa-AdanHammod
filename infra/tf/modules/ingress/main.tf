38b83784354613e6966ae38ea38ed0897a453c5d
Adan Hammod <adanhammod@gmail.com>
2026-08-04 13:18:39 +0300

    feat: add ALB ingress infrastructure

---
+++ infra/tf/modules/ingress/main.tf
@@ -0,0 +1,104 @@
+resource "aws_security_group" "alb" {
+  name_prefix = "adan-k8s-alb-sg-"
+  description = "Security group for the internet-facing ingress ALB"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "adan-k8s-alb-sg"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.alb.id
+  description       = "HTTP access from the internet"
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.alb.id
+  description       = "HTTPS access from the internet"
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  to_port     = 443
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "all_outbound" {
+  security_group_id = aws_security_group.alb.id
+  description       = "Allow all outbound traffic"
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_to_workers" {
+  security_group_id = var.cluster_security_group_id
+  description       = "Ingress-nginx NodePort access from the ALB"
+
+  referenced_security_group_id = aws_security_group.alb.id
+  from_port                    = 30080
+  to_port                      = 30080
+  ip_protocol                  = "tcp"
+}
+
+resource "aws_lb" "this" {
+  name               = "adan-k8s-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  subnets         = var.public_subnet_ids
+  security_groups = [aws_security_group.alb.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "adan-k8s-alb"
+  }
+}
+
+resource "aws_lb_target_group" "ingress" {
+  name        = "adan-k8s-ingress-tg"
+  port        = 30080
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = var.vpc_id
+
+  health_check {
+    protocol            = "HTTP"
+    path                = "/"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "adan-k8s-ingress-tg"
+  }
+}
+
+resource "aws_autoscaling_attachment" "workers" {
+  autoscaling_group_name = var.worker_asg_name
+  lb_target_group_arn    = aws_lb_target_group.ingress.arn
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ingress.arn
+  }
+}
--- infra/tf/outputs.tf
+++ infra/tf/outputs.tf
@@ -2,3 +2,23 @@ output "control_plane_public_ip" {
   description = "Public IP address of the Kubernetes control plane"
   value       = module.k8s_cluster.control_plane_public_ip
 }
+
+output "vpc_id" {
+  description = "ID of the Kubernetes VPC"
+  value       = module.k8s_cluster.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets used by the Kubernetes cluster"
+  value       = module.k8s_cluster.public_subnet_ids
+}
+
+output "worker_asg_name" {
+  description = "Name of the Kubernetes worker Auto Scaling Group"
+  value       = module.k8s_cluster.worker_asg_name
+}
+
+output "cluster_security_group_id" {
+  description = "ID of the shared Kubernetes cluster security group"
+  value       = module.k8s_cluster.cluster_security_group_id
+}
